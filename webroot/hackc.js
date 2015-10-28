e358e9db6f2f69f140192f02dc187518d17b4a08
Basile Starynkevitch <basile@starynkevitch.net>
2015-10-28 14:57:49 +0100

    adding hackc.js

---
+++ webroot/hackc.js
@@ -0,0 +1,61 @@
+// file hackc.js - Javascript & Jquery for hackc.html.
+
+/**   Copyright (C)  2015 Basile Starynkevitch, later FSF
+      MONIMELT is a monitor for MELT - see http://gcc-melt.org/
+      This file is part of GCC.
+      
+      GCC is free software; you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation; either version 3, or (at your option)
+      any later version.
+      
+      GCC is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+      You should have received a copy of the GNU General Public License
+      along with GCC; see the file COPYING3.   If not see
+      <http://www.gnu.org/licenses/>.
+**/
+
+var prologuetxa;
+var initialtxa;
+var prologuecodmir;
+var initialcodmir;
+
+function got_hackc_click(ev)
+{
+    console.debug ("got_hackc_click ev=", ev);
+    $.ajax(
+	{
+	    url: "/mom_hackc_code",
+	    method: "POST",
+	    data: {
+		do_hackc: "ajaxhack",
+		prologuetxt: prologuetxa.text(),
+		initialtxt: initialtxa.text()
+	    },
+	    dataType: "JSON",
+	    success: function (answer) {
+		console.debug ("hackc answer=", answer);
+	    }
+	});
+    
+};				// end got_hackc_click
+
+
+$(document).ready(function(){
+    prologuetxa = $("#prologuetxa_id");
+    initialtxa = $("#initialtxa_id");
+    prologuecodmir = CodeMirror.fromTextArea(prologuetxa[0], {
+	lineNumbers: true,
+	theme: "default",
+	mode: "text/x-csrc"
+    });
+    initialcodmir = CodeMirror.fromTextArea(initialtxa[0], {
+	lineNumbers: true,
+	theme: "neo",
+	mode: "text/x-csrc"
+    });
+    $("#hackc_id").click(got_hackc_click);
+});				// end document ready function
