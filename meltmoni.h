02c01a5f48a20009298cabdd8f8fd7c4a106f6a5
Basile Starynkevitch <basile@starynkevitch.net>
2015-11-23 15:18:57 +0100

    mom_set_items_prefixed works ok

--- meltmoni.h
+++ meltmoni.h
@@ -1199,7 +1199,7 @@ struct mom_item_st *mom_clone_item_from_radix (const struct mom_itemname_tu
 static inline struct mom_item_st *
 mom_clone_item (const struct mom_item_st *itm)
 {
-  if (itm)
+  if (itm && itm != MOM_EMPTY_SLOT)
     return mom_clone_item_from_radix (itm->itm_radix);
   return NULL;
 }
