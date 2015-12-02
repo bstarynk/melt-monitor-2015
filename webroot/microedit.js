22f0d309214a9f709ced052809b1025be3ef2215
Basile Starynkevitch <basile@starynkevitch.net>
2015-12-02 15:22:36 +0100

    recoding microedit.js....

--- webroot/microedit.js
+++ webroot/microedit.js
@@ -37,7 +37,7 @@ function htmlDecode(value){
     return $('<div/>').html(value).text();
 };
 
-var momc_count=0;
+var momc_ast_count=0;
 var momv_entriesvar=null;
 var mom_name_regexp = /^[A-Za-z0-9_]*$/;
 
@@ -105,11 +105,11 @@ function mom_complete_name(name) {
 
 
 
-// this utility function is assigning a unique number
-function mom_numbered(obj) {
-    momc_count = momc_count+1;
-    obj.inum = momc_count;
-    return momc_count;
+// this utility function is assigning a unique number for ASTs
+function mom_ast_numbered(obj) {
+    momc_ast_count = momc_ast_count+1;
+    obj.mom_ast_inum = momc_ast_count;
+    return momc_ast_count;
 };
 /// this function is called by /microedit AJAX for do_fillpage at document loading
 function mom_ajaxfillscript(script) {
@@ -156,7 +156,7 @@ function MomeEntry(entitm,entval) {
     this.mom_entry_item= entitm;
     this.mom_entry_val= entval;
     this.mom_ast = "entry";
-    mom_numbered(this);
+    mom_ast_numbered(this);
 };
 var momp_entry = {
     name: "MomeEntry",
@@ -191,7 +191,7 @@ var momp_item_ref = {
 function MomeItemRef(iname) {
     this.mom_item_name = iname;
     this.mom_ast = "item_ref";
-    mom_numbered(this);
+    mom_ast_numbered(this);
 };
 MomeItemRef.prototype = momp_item_ref;
 function mome_item_ref(itmname) {
@@ -209,7 +209,7 @@ var momp_nil_value = {
     }
 };
 function MomeNilValue() {
-    mom_numbered(this);
+    mom_ast_numbered(this);
     this.mom_ast = "nilval";
 };
 MomeNilValue.prototype = momp_nil_value;
@@ -228,7 +228,7 @@ var momp_nil_ref = {
     }
 };
 function MomeNilRef() {
-    mom_numbered(this);
+    mom_ast_numbered(this);
     this.mom_ast = "nilref";
 };
 MomeNilRef.prototype = momp_nil_ref;
@@ -240,4 +240,189 @@ function mome_nil_ref() {       /// a nil item reference
 };
 ////////////////
 
+var momp_item_value = {
+    name: "MomeItemValue",
+    realize: function (incont,fromast) {
+	console.error("MomeItemValue-realize unimplemented incont=", incont, " fromast=", fromast);
+    }
+};
+function MomeItemValue(iname) {
+    console.assert (typeof(iname)==='string',
+		    "MomeItemValue bad iname=", iname);
+    mom_ast_numbered(this);
+    this.mom_item_name = iname;
+    this.mom_ast = "itemval";
+};
+MomeItemValue.prototype = momp_item_value;
+
+function mome_item_val(itemname) { /// a nil value
+    var res = new MomeItemValue(itemname);
+    console.log ("mome_item_val res=", res);
+    return res;
+};
+
+
+////////////////
+var momp_number_value = {
+    name: "momp-number-value",
+    realize:function (incont,fromast) {
+	console.error("momp-number-value-realize unimplemented incont=", incont, " fromast=", fromast);
+    }
+}
+////////////////
+var momp_int_value = {
+    name: "MomeIntValue",
+    __proto__: momp_number_value
+};
+function MomeIntValue(ival) {
+    console.assert (typeof ival === 'number',
+		    "MomeIntValue check ival is number");
+    mom_ast_numbered(this);
+    this.mom_num_val = ival;
+};
+MomeIntValue.prototype = momp_int_value;
+
+function mome_int (num) {
+    var res = new MomeIntValue(num);
+    console.log ("mome_int res=", res);
+    return res;
+};
+
+////////////////
+var momp_double_value = {
+    name: "MomeDoubleValue",
+    __proto__: momp_number_value
+};
+function MomeDoubleValue(ival) {
+    console.assert (typeof ival === 'number',
+		    "MomeDoubleValue check ival is number");
+    mom_ast_numbered(this);
+    this.mom_num_val = ival;
+};
+MomeDoubleValue.prototype = momp_double_value;
+
+function mome_double (num) {
+    var res = new MomeDoubleValue(num);
+    console.log ("mome_double res=", res);
+    return res;
+};
+
+////////////////
+var momp_string_value = {
+    name: 'MomeStringValue',
+    __proto__: momp_value,
+    realize: function (incont,fromast) {
+	console.error("MomeStringValue-realize unimplemented incont=", incont, " fromast=", fromast);
+    }
+};
+function MomeStringValue(sval) {
+    console.assert (typeof sval === 'string',
+		    "MomeStringValue check sval is number");
+    mom_ast_numbered(this);
+    this.string_val = sval;
+};
+MomeStringValue.prototype = momp_string_value;
+
+function mome_string (str) {
+    var res = new MomeStringValue(str);
+    console.log ("mome_string res=", str);
+    return res;
+};
+
+
+////////////////
+var momp_sequence_value = {
+    name: 'momp-sequence-value',
+    __proto__: momp_value,
+    realize: function (incont,fromast) {
+	console.error("momp-sequence-value-realize unimplemented incont=", incont, " fromast=", fromast);
+    }
+};
+    
+////////////////
+var momp_tuple_value = {
+    name: 'MomeTupleValue',
+    __proto__: momp_sequence_value
+};
+function MomeTupleValue(tval) {
+    console.assert (typeof tval === 'array',
+		    "MomeTupleValue check tval is array");
+    mom_ast_numbered(this);
+    this.mom_seq = tval;
+};
+MomeTupleValue.prototype = momp_tuple_value;
+
+function mome_tuple (arr) {
+    var res = new MomeTupleValue(arr);
+    console.log ("mome_tuple res=", str);
+    return res;
+};
+    
+////////////////
+var momp_set_value = {
+    name: 'MomeTupleValue',
+    __proto__: momp_sequence_value
+};
+function MomeSetValue(tval) {
+    console.assert (typeof tval === 'array',
+		    "MomeSetValue check tval is array");
+    mom_ast_numbered(this);
+    this.mom_seq = tval;
+};
+MomeSetValue.prototype = momp_tuple_value;
+
+function mome_set (arr) {
+    var res = new MomeSetValue(arr);
+    console.log ("mome_set res=", str);
+    return res;
+};
+    
+////////////////
+var momp_node_value = {
+    __proto__: momp_value,
+    name: "MomeNodeValue",
+    realize: function (incont,fromast) {
+	console.error("MomeNodeValue-realize unimplemented incont=", incont, " fromast=", fromast);
+    }
+};
+function MomeNodeValue(conn,sons) {
+    mom_ast_numbered(this);
+    console.assert(typeof conn==='object', "MomeNodeValue bad conn=", conn);
+    console.assert(typeof sons==='array', "MomeNodeValue bad sons=", sons);
+    this.mom_conn = conn;
+    this.mom_sons = sons;
+}
+function mome_node(connitm, sonsarr) {
+    var res = new MomeNodeValue(connitm,sonsarr);
+    console.log ("mome_node res=", res);
+    return res;
+};
+////////////////////////////////////////////////////////////////
+
+
+
+$(document).ready(function(){
+    console.log("microedit document ready");
+    // see http://stackoverflow.com/a/10556743/841108
+    window.onerror = function(msg,url,line,col,error) {
+        console.error("window error ", url, ":", line, ": ", msg, " /", error);
+    };
+    $editdiv = $("#microedit_id");
+    $editlog = $("#editlog_id");
+    $cleareditbut = $("#cleareditbut_id");
+    console.log ("microedit readying $editdiv=", $editdiv, " $editlog=", $editlog, " $cleareditbut=", $cleareditbut);
+    $cleareditbut.click(function(evt){
+        console.log("clearedit evt=", evt);
+        $editlog.html("");
+    });
+    console.log("microedit before ajax do_fillpage");
+    $.ajax
+    ({url: "/microedit",
+      method: "POST",
+      data: {"do_fillpage": true},
+      dataType: "script",
+      success: mom_ajaxfillscript
+     });
+    console.log("microedit document done ready");
+});
 console.warn("incomplete new microedit.js, see also _old_microedit.js");
